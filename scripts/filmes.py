a596cd6c44043130139071cd471f8335f675b5cc
r1ckhdk <rick@riseup.net>
2022-02-07 16:49:50 -0300

    create main function

--- scripts/filmes.py
+++ scripts/filmes.py
@@ -1,32 +1,37 @@
 import csv
 import os
 
-dir_filmes = '(diretorio_pasta_filmes)'
+def main():
+    dir_filmes = '(diretorio_pasta_filmes)'
 
-# Busca o nome dos diretórios e os armazena na lista filmes
-filmes = os.listdir(dir_filmes)
+    # Busca o nome dos diretórios e os armazena na lista filmes
+    filmes = os.listdir(dir_filmes)
 
-# Reordena os filmes em ordem alfabética
-filmes.sort(key=str.casefold)
+    # Reordena os filmes em ordem alfabética
+    filmes.sort(key=str.casefold)
 
-ano_filme = []
-titulo_filme = []
+    ano_filme = []
+    titulo_filme = []
 
-# Armazena somente o ano de cada filme na lista ano_filme
-for a in filmes:
-    ano_filme.append(a[-5:-1])
+    # Armazena somente o ano de cada filme na lista ano_filme
+    for a in filmes:
+        ano_filme.append(a[-5:-1])
 
-# Armazena somente o título de cada filme na lista titulo_filme
-for t in filmes:
-    titulo_filme.append(t[:-7])
+    # Armazena somente o título de cada filme na lista titulo_filme
+    for t in filmes:
+        titulo_filme.append(t[:-7])
 
 
-rows = zip(titulo_filme, ano_filme)
+    rows = zip(titulo_filme, ano_filme)
 
-# Em uma planilha csv, escreve cada item das listas em uma coluna separada
-dir_csv = '(diretorio_output_csv).csv'
-with open(dir_csv, 'w', encoding='UTF-8') as filmes_csv:
-    writer = csv.writer(filmes_csv)
-    for row in rows:
-        writer.writerow(row)
-    filmes_csv.close()
+    # Em uma planilha csv, escreve cada item das listas em uma coluna separada
+    dir_csv = '(diretorio_output_csv).csv'
+    with open(dir_csv, 'w', encoding='UTF-8') as filmes_csv:
+        writer = csv.writer(filmes_csv)
+        for row in rows:
+            writer.writerow(row)
+        filmes_csv.close()
+
+
+if __name__ == '__main__':
+    main()
